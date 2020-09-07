6a81c1d491fb9fb6793493c35244550ee6c0d9ad
Daniel Soares <danysoares27@gmail.com>
2020-09-07 17:06:00 -0600

    added data preprocessor

---
+++ final/preProcessor.c
@@ -0,0 +1,73 @@
+/*
+ * Preprocessor for Netflix movie reviews
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    //Hardcode how many movies you want to look at since there's a lot
+    int nMovies = 100;
+    //Initialize a 2d array of users x movies with all ratings set to 0
+    int ratings[2700000][17770] = {0};
+    char movieNames[17770];
+    char prefix[8] = "./data/";
+    //Get movie titles
+    FILE *fPointer;
+    fPointer = fopen(strcat(prefix, "movie_titles.txt"), "r");
+
+    int id;
+    int date;
+    char title[150];
+    // read line by line till end of file
+    while( fscanf(fPointer, "%[^,],%[^,],%s\n", &id, &date, title) == 1 ) {
+        movieNames[id] = title;
+    }
+    fclose(fPointer);
+
+
+    //Get movie reviews
+    for (int i = 1; i < nMovies + 1; i++) {
+        char file[10] = strcat(i, ".txt");
+        char prefix2[30];
+        if (i < 10) {
+            prefix2[30] = "./data/my_000000";
+        }
+        else if (i < 100) {
+            prefix2[30] = "./data/my_00000";
+        }
+        else if (i < 1000) {
+            prefix2[30] = "./data/my_0000";
+        }
+        else {
+            prefix2[30] = "./data/my_000";
+        }
+
+        strcat(prefix2, file);
+        FILE *fPointer2;
+        fPointer2 = fopen(prefix2, "r");
+
+        char singleLine[150];
+        int userId;
+        int rating;
+        bool firstRow = true;
+        // read line by line till end of file
+        while( fscanf(fPointer2, "%[^,],%[^,],%s\n", &userId, &rating) == 1 ) {
+            //Check for exess user id, skip first row
+            if (!firstRow) {
+                if ( userId > 2700000 ) {
+                    printf('Add more users! User id: %d', userId);
+                } else {
+                    ratings[userId][i] = rating;
+                }
+            }
+            if (firstRow == true) {
+                firstRow = false;
+            }
+        }
+        fclose(fPointer2);
+    }
+
+    return ratings;
+}
